0de6f98b394e6afe349d4e9d1684d1d583ac3a21
happyprogfrog <happyprogfrog@gmail.com>
2024-07-10 15:44:40 +0900

    feat: User 정보를 가져올 때 Redis를 사용하도록 개선 1
    - userRedisTemplate 사용

--- src/main/java/me/progfrog/cache_study2/service/UserService.java
+++ src/main/java/me/progfrog/cache_study2/service/UserService.java
@@ -3,15 +3,30 @@ package me.progfrog.cache_study2.service;
 import lombok.RequiredArgsConstructor;
 import me.progfrog.cache_study2.domain.entity.User;
 import me.progfrog.cache_study2.repository.UserRepository;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final RedisTemplate<String, User> userRedisTemplate;
 
     public User getUser(final Long id) {
-        return userRepository.findById(id).orElseThrow();
+        var key = "users:%d".formatted(id);
+
+        // 1. cache get
+        var cachedUser = userRedisTemplate.opsForValue().get(key);
+        if (cachedUser != null) {
+            return cachedUser;
+        }
+
+        // 2. else db -> cache set
+        User user = userRepository.findById(id).orElseThrow();
+        userRedisTemplate.opsForValue().set(key, user, Duration.ofSeconds(30));
+        return user;
     }
 }
