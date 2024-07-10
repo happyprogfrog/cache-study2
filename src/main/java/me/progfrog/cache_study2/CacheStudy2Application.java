b8152cc643a37dd74a94f8f163ec7e218df70656
happyprogfrog <happyprogfrog@gmail.com>
2024-07-10 15:09:08 +0900

    프로젝트 생성

---
+++ src/main/java/me/progfrog/cache_study2/CacheStudy2Application.java
@@ -0,0 +1,13 @@
+package me.progfrog.cache_study2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheStudy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheStudy2Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/progfrog/cache_study2/CacheStudy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.progfrog.cache_study2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheStudy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
